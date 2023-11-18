11c009c0520cdcb2af820530c00eed0adad28d95
Joseph Mearman <joseph@mearman.co.uk>
2023-11-18 03:46:09 +0000

    Add Hello World
    It doesn't get much more minimal than this

---
+++ samples/Hello World/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env npx -y tsx watch
+
+import { Board } from "@google-labs/breadboard";
+
+const board = new Board();
+
+(async () => {
+	board.input().wire("*", board.output());
+	console.log(
+		await board.runOnce({
+			message: "Hello Breadboard!",
+		})
+	);
+})();
