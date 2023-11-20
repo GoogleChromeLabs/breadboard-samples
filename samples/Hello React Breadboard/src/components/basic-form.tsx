ba0ff877b3048c97838bd611ce465dc83e232381
a.mitu <a.mitu@sourcepulp.com>
2023-11-20 16:03:17 +0000

    tidy up

--- samples/Hello React Breadboard/src/components/basic-form.tsx
+++ samples/Hello React Breadboard/src/components/basic-form.tsx
@@ -2,74 +2,37 @@ import { Button, Card, Form, Input } from "antd";
 import { Board } from "@google-labs/breadboard";
 import { useState } from "react";
 import OutputCard, { OutputCardProps } from "./output-card";
+import mainStyles from "../styles/main.module.scss";
 
 type FieldType = {
-	// id: string;
-	// blah: string;
-	// nodeId: string;
 	input: string;
-	// inputTwoParam: string;
 };
 
-// const inputNodeOne = {
-// 	id: "input-one",
-// 	name: "partOne",
-// };
-
-// const inputNodeTwo = {
-// 	id: "input-two",
-// 	name: "partTwo",
-// };
-
-const inputAttribute = "message";
-
 const BasicForm = (): React.JSX.Element => {
-	// const [nodeState, setNodeOutput] = useState<OutputCardProps>({ key: wireAttribute, output: {} });
 	const [nodeState, setNodeState] = useState<OutputCardProps>({
 		nodeData: {},
 	});
-	console.log("nodeOutput", JSON.stringify(nodeState, null, 2));
+
 	const board = new Board({
-		title: "new board",
+		title: "Basic board",
 	});
 
+	const inputAttribute = "message";
+
 	const onSubmit = async (values: FieldType) => {
+		//#region Board Wiring
 		const input = board.input();
 		const output = board.output();
-
 		input.wire(inputAttribute, output);
+		//#endregion
 
-		// const inputOne = board.input({
-		// 	$id: inputNodeOne.id,
-		// });
-		// inputOne.wire(inputNodeOne.name, output);
-
-		// const inputTwo = board.input({
-		// 	$id: inputNodeTwo.id,
-		// });
-		// inputTwo.wire(inputNodeTwo.name, output);
-
+		//#region Board run logic
 		for await (const run of board.run()) {
-			// console.log("run");
 			if (run.type === "input") {
 				run.inputs = {
 					[inputAttribute]: values.input,
 				};
-				// if (run.node.id == inputNodeOne.id) {
-				// 	run.inputs = {
-				// 		[inputNodeOne.name]: values.inputOneParam
-				// 	}
-				// }
-				// if (run.node.id == inputNodeTwo.id) {
-				// 	run.inputs = {
-				// 		[inputNodeTwo.name]: values.inputTwoParam
-				// 	}
-				// }
-				// run.inputs = {
-				// 	message: `The post ID is ${values.id}!`,
-				// };
 			} else if (run.type === "output") {
-				// setNodeOutput(JSON.stringify(run.outputs, null, 2));
 				setNodeState({
 					nodeData: {
 						node: run.node,
@@ -79,6 +42,7 @@ const BasicForm = (): React.JSX.Element => {
 				});
 			}
 		}
+		//#endregion
 	};
 
 	return (
@@ -86,12 +50,7 @@ const BasicForm = (): React.JSX.Element => {
 			<Card title="Submit post ID">
 				<Form
 					name="basic"
-					style={{
-						maxWidth: 600,
-						display: "flex",
-						alignItems: "center",
-						margin: "3em",
-					}}
+					className={mainStyles.basicForm}
 					initialValues={{ remember: true }}
 					autoComplete="off"
 					onFinish={onSubmit}
@@ -103,14 +62,6 @@ const BasicForm = (): React.JSX.Element => {
 					>
 						<Input />
 					</Form.Item>
-
-					{/* <Form.Item<FieldType>
-					label="Node ID"
-					name="nodeId"
-					rules={[{ required: false }]}
-				>
-					<Input /> */}
-					{/* </Form.Item> */}
 					<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
 						<Button type="primary" htmlType="submit">
 							Submit
--- samples/Hello React Breadboard/src/components/output-card.tsx
+++ samples/Hello React Breadboard/src/components/output-card.tsx
@@ -1,29 +1,20 @@
 import { Card } from "antd";
 import { OutputValues } from "@google-labs/breadboard";
+import mainStyles from "../styles/main.module.scss";
 
 export type OutputCardProps = {
 	nodeData: OutputValues;
-	// attribute: string;
 };
 
 const OutputCard = ({ nodeData }: OutputCardProps): React.JSX.Element => {
-	// const parsedOutput = JSON.parse(output);
 	const outputString = JSON.stringify(nodeData, null, 2);
-	console.log(outputString);
-	// console.log("key", attribute);
-	// console.log("output", output);
-	// const builtString = `${attribute}: ${output[attribute]}`;
-	// console.log("builtString", builtString);
-	// console.log(JSON.stringify(output, null, 2));
 
 	return (
 		<Card
 			title="Output"
-			style={{
-				width: "300px",
-			}}
+			className={mainStyles.outputCard}
 		>
-			{outputString}
+			<pre className={mainStyles.outputContent}>{outputString}</pre>
 		</Card>
 	);
 };
