adbfa43358b50931f719bd3b92b4bb118eadbd41
Joseph Mearman <joseph@mearman.co.uk>
2023-11-25 07:57:13 +0000

    Add VitePWA plugin for automatic service worker updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		tsconfigPaths()
+		tsconfigPaths(),
+		VitePWA({
+			registerType: "autoUpdate",
+			strategies: "injectManifest",
+			injectRegister: "auto",
+			devOptions: {
+				enabled: true,
+			},
+			srcDir: "src",
+			filename: "sw/worker.ts",
 	],
 });
