7e26fba1db9d41c8a3bc38495715d2fbfb7c6e45
Joseph Mearman <joseph@mearman.co.uk>
2023-11-26 12:16:31 +0000

    feat: Add service worker registration
    This commit adds code to register a service worker in the main.tsx file. The service worker is registered based on the environment mode, either using "/sw/worker.js" for production or "/dev-sw.js?dev-sw" for development. The type of the registration is also determined by the environment mode, with "classic" used for production and "module" used for development.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker.register(
+		import.meta.env.MODE === "production"
+			? "/sw/worker.js"
+			: "/dev-sw.js?dev-sw",
+		{ type: import.meta.env.MODE === "production" ? "classic" : "module" }
+	);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<App />
