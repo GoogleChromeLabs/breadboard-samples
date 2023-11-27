145fb4cf32971a0b74a6ae5c32ce7b343345233a
Joseph Mearman <joseph@mearman.co.uk>
2023-11-27 12:26:19 +0000

    Add HackerNewsAlgoliaKit and HackerNewsFirebaseKit to makeBoard

--- src/sw/makeBoard.ts
+++ src/sw/makeBoard.ts
@@ -1,19 +1,65 @@
 import { Board } from "@google-labs/breadboard";
+import {
+	HackerNewsAlgoliaKit,
+	HackerNewsFirebaseKit,
+	ListKit,
+} from "@exadev/breadboard-kits";
+import Core from "@google-labs/core-kit";
 
 export function makeBoard(): Board {
 	const board = new Board();
-	const input1 = board.input({ $id: "input1" });
-	const input2 = board.input({ $id: "input2" });
+	//////////////////////////////////////////////
+	const hnFirebaseKit = board.addKit(HackerNewsFirebaseKit);
+	const core = board.addKit(Core);
+	const listKit = board.addKit(ListKit);
+	//////////////////////////////////////////////
+	const hackerNewsTopStoryIdList = core.passthrough();
+	hnFirebaseKit.topStoryIds().wire("storyIds", hackerNewsTopStoryIdList);
 
-	const output1 = board.output({ $id: "output1" });
-	input1.wire("msgOne", output1);
+	const popStory = listKit.pop({
+		$id: "popStoryId",
+	});
+	hackerNewsTopStoryIdList.wire("storyIds->list", popStory);
+	popStory.wire("->list", popStory);
+	const storyId = core.passthrough();
+	popStory.wire("item->id", storyId);
 
-	const output2 = board.output({ $id: "output2" });
-	input2.wire("msgTwo", output2);
+	storyId.wire(
+		"id",
+		board.output({
+			$id: "storyId",
+		})
+	);
+	//////////////////////////////////////////////
+	const hnAlgoliaKit = board.addKit(HackerNewsAlgoliaKit);
 
-	const combinedOutput = board.output({ $id: "combinedOutput" });
-	input1.wire("msgOne->partOne", combinedOutput);
-	input2.wire("msgTwo->partTwo", combinedOutput);
+	const getStoryFromId = hnAlgoliaKit.getStory();
+	storyId.wire("id", getStoryFromId);
+	const story = core.passthrough();
 
+	getStoryFromId.wire("*", story);
+	story.wire(
+		"*",
+		board.output({
+			$id: "fullStory",
+		})
+	);
+
+	const storyOutput = board.output({
+		$id: "story",
+	});
+	story.wire("algoliaUrl", storyOutput);
+	story.wire("author", storyOutput);
+	story.wire("children", storyOutput);
+	story.wire("created_at", storyOutput);
+	story.wire("created_at_i", storyOutput);
+	// story.wire("id", storyOutput);
+	story.wire("points", storyOutput);
+	story.wire("story_id", storyOutput);
+	story.wire("title", storyOutput);
+	// story.wire("type", storyOutput);
+	story.wire("url", storyOutput);
+
+	//////////////////////////////////////////////
 	return board;
 }
