23e24040bdf6f7e57a00cb1a1862ab6e07185304
a.mitu <a.mitu@sourcepulp.com>
2023-12-01 15:53:03 +0000

    show loader when search is in running state

--- src/components/OutputCard.tsx
+++ src/components/OutputCard.tsx
@@ -2,6 +2,8 @@ import { intlFormatDate } from "~/hooks/useFormattedDate";
 import "./OutputCard.css";
 import { OutputNodeData } from "~/sw/types";
 import useCategoriseNodes from "~/hooks/useCategoriseNodes";
+import useWorkerController from "~/worker/useWorkerController";
+import { Spin } from "antd";
 //import useComponentForNode from "~/hooks/useComponentForNode";
 
 export type OutputNodeProps = {
@@ -11,6 +13,8 @@ export type OutputNodeProps = {
 
 const OutputNode = ({ data }: OutputNodeProps): React.JSX.Element => {
 
+	const { status } = useWorkerController();
+
 	const dataString = JSON.stringify(data, null, 2);
 	const dataObject = JSON.parse(dataString);
 
@@ -18,64 +22,71 @@ const OutputNode = ({ data }: OutputNodeProps): React.JSX.Element => {
 
 	const categoryArrays = useCategoriseNodes(dataObject);
 
-	/* 	const getSummaries = (searchData: OutputNodeData[], summaries: OutputNodeData[]): OutputNodeData | undefined => {
-			let matchSummary;
-			summaries.map((summary) => {
-				matchSummary = searchData.some((data) => data.output.story_id === summary.output.story_id)
-			})
-			console.log(matchSummary);
-			return matchSummary;
-		};
-	
-		console.log(getSummaries(categoryArrays.searchResultData, categoryArrays.summarisationData)); */
+	/* const getSummaries = (searchData: OutputNodeData[], summaries: OutputNodeData[]): OutputNodeData | undefined => {
+		let matchSummary;
+		summaries.map((summary) => {
+			matchSummary = searchData.filter((data) => data.output.story_id === summary.output.story_id)
+		})
+		console.log(matchSummary);
+		return matchSummary;
+	};
+
+	console.log(getSummaries(categoryArrays.searchResultData, categoryArrays.summarisationData)); */
 
 
 	return (
-		<div>
-			{
-				categoryArrays.searchResultData.map((result) => (
-					<section className="card">
-						<h3>{result.output.title}</h3>
-						<h4>By {result.output.author}</h4>
-						<p>URL:<a href={result.output.url}>{result.output.url}</a></p>
-						<p>Created at: {intlFormatDate(result.output.created_at)}</p>
-					</section>
-				))
+		<>
+			{status === "finished" ? (<div>
+				{
+					categoryArrays.searchResultData.map((result) => (
+						<section className="card">
+							<h3>{result.output.title}</h3>
+							<h4>By {result.output.author}</h4>
+							<p>URL:<a href={result.output.url}>{result.output.url}</a></p>
+							<p>Created at: {intlFormatDate(result.output.created_at)}</p>
+						</section>
+					))
 
-			}
-			{
-				categoryArrays.summarisationData.map((result) => (
-					<section className="card">
-						<h4>Type: {result.node}</h4>
-						<p>URL:<a href={result.output.url}>{result.output.url}</a></p>
-						<p>Completion: {result.output.completion}</p>
-						<p>Created at: {intlFormatDate(result.output.created_at)}</p>
-					</section>
-				))
+				}
+				{
+					categoryArrays.summarisationData.map((result) => (
+						<section className="card">
+							<h4>Type: {result.node}</h4>
+							<p>URL:<a href={result.output.url}>{result.output.url}</a></p>
+							<p>Completion: {result.output.completion}</p>
+							<p>Created at: {intlFormatDate(result.output.created_at)}</p>
+						</section>
+					))
 
-			}
-			{
-				categoryArrays.debugData.map((result) => (
-					<pre
-						style={{
-							backgroundColor: "black",
-							whiteSpace: "pre-wrap",
-							margin: "10px",
-							padding: "20px",
-							color: "#00FF00",
-							fontFamily: 'Consolas, "Courier New", monospace',
-							borderRadius: "15px",
-							boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
-							overflow: "auto",
-							textAlign: "left",
-						}}>
-						{JSON.stringify(result, null, 2)}
-					</pre>
-				))
+				}
+				{
+					categoryArrays.debugData.map((result) => (
+						<pre
+							style={{
+								backgroundColor: "black",
+								whiteSpace: "pre-wrap",
+								margin: "10px",
+								padding: "20px",
+								color: "#00FF00",
+								fontFamily: 'Consolas, "Courier New", monospace',
+								borderRadius: "15px",
+								boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+								overflow: "auto",
+								textAlign: "left",
+							}}>
+							{JSON.stringify(result, null, 2)}
+						</pre>
+					))
 
-			}
-		</div>);
+				}
+			</div>) :
+				(<Spin tip="Loading" size="large">
+					<div className="content" />
+				</Spin>)}
+		</>
+	);
 
 };
 
 export default OutputNode;
+
