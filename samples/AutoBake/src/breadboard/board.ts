9d5e1096aefdcef5b4942674d130387926e8729c
Joseph Mearman <joseph@mearman.co.uk>
2023-12-04 21:07:23 +0000

    Add Chrome Status Kit and List Kit to AutoBake board

--- samples/AutoBake/src/breadboard/board.ts
+++ samples/AutoBake/src/breadboard/board.ts
@@ -1,16 +1,46 @@
+import { ListKit } from "@exadev/breadboard-kits";
 import generateAndWriteCombinedMarkdown from "@exadev/breadboard-kits/util/files/generateAndWriteCombinedMarkdown";
-import { Board } from "@google-labs/breadboard";
+import { Board, OutputValues } from "@google-labs/breadboard";
+import { KitBuilder } from "@google-labs/breadboard/kits";
+import chromeStatusApiFeatures from "./chromeStatusApiFeatures";
+import { chromeStatusFeaturesV2 } from "./chromeStatusFeaturesV2";
+import { chromeVersions } from "./chromeVersions";
+
+const chromeStatusKitBuilder = new KitBuilder({
+	title: "Chrome Status",
+	url: "npm:@exadev/breadboard-kits/chrome-status",
+}).build({
+	async versions(): Promise<OutputValues> {
+		return chromeVersions();
+	},
+	async chromeStatusApiFeatures(): Promise<OutputValues> {
+		return chromeStatusApiFeatures();
+	},
+	async chromeStatusFeaturesV2(): Promise<OutputValues> {
+		return { features: await chromeStatusFeaturesV2() };
+	},
+});
 
 const board = new Board({
 	title: "AutoBake",
 });
 
-board.input().wire(
-	"*",
-	board.output({
-		message: "Hello World",
-	})
-);
+//////////////////////////////////////////////////
+const chromeStatusKit = board.addKit(chromeStatusKitBuilder);
+const listKit = board.addKit(ListKit);
+//////////////////////////////////////////////////
+const chromeVersionsNode = chromeStatusKit.versions();
+chromeVersionsNode.wire("*", board.output());
+//////////////////////////////////////////////////
+const chromeStatusApiFeaturesNode = chromeStatusKit.chromeStatusApiFeatures();
+const popV1Features = listKit.pop();
+chromeStatusApiFeaturesNode.wire("features->list", popV1Features);
+popV1Features.wire("item", board.output());
+//////////////////////////////////////////////////
+const chromeStatusFeaturesV2Node = chromeStatusKit.chromeStatusFeaturesV2();
+const popV2Features = listKit.pop();
+chromeStatusFeaturesV2Node.wire("features->list", popV2Features);
+popV2Features.wire("item", board.output());
 
 generateAndWriteCombinedMarkdown({
 	board,
