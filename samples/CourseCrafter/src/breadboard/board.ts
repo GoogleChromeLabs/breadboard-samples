b72a483b31f08a78300ae9a85cf0920a4e0c0160
Joseph Mearman <joseph@mearman.co.uk>
2023-12-08 09:59:27 +0000

    Add CourseCrafterKit to the board

--- samples/CourseCrafter/src/breadboard/board.ts
+++ samples/CourseCrafter/src/breadboard/board.ts
@@ -1,8 +1,12 @@
 import { Board } from "@google-labs/breadboard";
+import { CourseCrafterKit } from "@exadev/breadboard-kits";
 
 const board = new Board({
 	title: "BourseCrafter",
 });
+
+const couseCrafter = board.addKit(CourseCrafterKit);
+
 const input = board.input({ input: "input" });
 const output = board.output({ output: "output" });
 input.wire("*", output);
