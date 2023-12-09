f1454f7e8fbefe02a8a576d1a446ff52c7b8c19b
Joseph Mearman <joseph@mearman.co.uk>
2023-12-09 11:38:57 +0000

    add dev-dist/ to .gitignore

--- samples/DevPulse/DevPulse React/dev-dist/registerSW.js
+++
@@ -1,6 +0,0 @@
-// if ("serviceWorker" in navigator)
-// 	navigator.serviceWorker
-// 		.register("/dev-sw.js?dev-sw", { scope: "/", type: "classic" })
-// 		.catch(function (err) {
-// 			console.error("Service Worker registration failed: ", err);
-// 		});
