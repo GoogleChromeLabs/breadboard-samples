6bb4a10981d293d3594e1f16b5b4862df3a29838
jig925 <57284752+jig925@users.noreply.github.com>
2023-12-14 14:03:40 +0000

    WIP
    started to implement kit which allows users to  pick a feature

--- samples/AutoBake/src/breadboard/featurekit.ts
+++ samples/AutoBake/src/breadboard/featurekit.ts
@@ -1,10 +1,9 @@
 import { KitBuilder } from "@google-labs/breadboard/kits";
-import {InputValues, NodeValue} from "@google-labs/breadboard";
-
+import { InputValues, NodeValue } from "@google-labs/breadboard";
 import { list } from "@exadev/breadboard-kits/types";
-
 import axios from "axios";
 import * as cheerio from "cheerio";
+import * as readline from 'readline/promises';
 
 type featureContents = {
     overview: NodeValue
@@ -13,11 +12,15 @@ type featureContents = {
 type feature = {
     id: NodeValue,
     name: NodeValue,
-    summary: NodeValue
+    summary: NodeValue,
+    category: NodeValue
+}
+
+type featureOutput = {
+    features: feature[]
 }
 
 export type getBlogsHTMLContentInput = list.List;
-
 // based on stuff selected by user, extract the feature page and display data...
 async function extractFeatureContents(id: string): Promise<featureContents> {
     const baseURL = "https://chromestatus.com/feature/"
@@ -25,43 +28,70 @@ async function extractFeatureContents(id: string): Promise<featureContents> {
 
     const axiosInstance = axios.create()
     console.log(`Extracting Content from: ${featureURL}`)
-	const response = await axiosInstance.get(featureURL)
+    const response = await axiosInstance.get(featureURL)
 
 
     const selector = cheerio.load(response.data)
     const contents = selector("body").html()
 
-    return Promise.resolve({overview:contents})
+    return Promise.resolve({ overview: contents })
 }
 
 export const FeatureKit = new KitBuilder({
-	url: "npm@exadev/breadboard-kits/featureKit"
+    url: "npm@exadev/breadboard-kits/featureKit"
 }).build({
     async extractFunction(input: InputValues): Promise<void> {
-        const {list} : list.List = input as list.List;
+        const { list }: list.List = input as list.List;
 
-        for (const url of list){
+        for (const url of list) {
             const response = await extractFeatureContents(url as string)
 
-            console.log("RESPONSE", response)
+            console.log(response)
         }
-        
     },
     // parse the dirty json stuff, so we can display it to the user and they can select which one they want
     // by id
-    async parseFunctionJSON(input: InputValues): Promise<void> {
-        const {list} : list.List = input as list.List;
+    async parseFeatureJSON(input: InputValues): Promise<featureOutput> {
+        const { list }: list.List = input as list.List;
+        const features: Array<feature> = []
 
-        const features : Array<feature> = []
-
-        for (const jsonString of list){
-            const json = JSON.parse(jsonString as string); 
-            const feature = {id:json["id"], name:json["name"], summary:json["summary"]}
+        for (const jsonString of list) {
+            const json = JSON.parse(jsonString as string);
+            const feature = { id: json["id"], name: json["name"], summary: json["summary"], category: json["category"] }
             features.push(feature)
         }
 
-        console.log(features)
-    }
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+
+        try {
+            for (const feature of features) {
+                console.log(feature)
+            }
+
+            const answer = await rl.question('Please select an feature id to extract ', {
+                signal: AbortSignal.timeout(10_000) // 10s timeout
+            });
+
+            switch (answer.toLowerCase()) {
+                case 'y':
+                    console.log('Super!');
+                    break;
+                case 'n':
+                    console.log('Sorry! :(');
+                    break;
+                default:
+                    console.log('Invalid answer!');
+            }
+        } finally {
+            rl.close();
+        }
+
+        return Promise.resolve({ features })
+    },
 })
 
 export type FeatureKit = InstanceType<typeof FeatureKit>;
--- samples/AutoBake/src/breadboard/test.ts
+++ samples/AutoBake/src/breadboard/test.ts
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { getChromeStatusApiFeatures } from "./chromeStatusApiFeatures"
+import chromeStatusFeaturesV2 from "./chromeStatusFeaturesV2"
 import {FeatureKit} from "./featurekit"
-
 import { Board } from "@google-labs/breadboard"
 
-
 const board = new Board({title: "AutoBake"})
-
-
 const featureKit = board.addKit(FeatureKit)
-const getContents = featureKit.extractFunction()
+const parseJson = featureKit.parseFeatureJSON()
 
 const ids = board.input({
     $id: "featureIds",
@@ -24,14 +22,28 @@ const ids = board.input({
     },
 });
 
-const id = "5181611197071360"
 
-const featureIds = [id]
+ids.wire("list", parseJson)
 
-ids.wire("list->", getContents)
+const features = await getChromeStatusApiFeatures()
+const featureStrings = []
 
-getContents.wire("*", board.output())
+const featuresV2 = await chromeStatusFeaturesV2()
+const featureV2Strings = []
+
+for(const feature of features){
+    const my_string = JSON.stringify(feature)
+
+    featureStrings.push(my_string)
+}
+
+for(const feature of featuresV2){
+    const my_string = JSON.stringify(feature)
+    featureV2Strings.push(my_string)
+}
 
 await board.runOnce({
-    list: featureIds
+    list: featureStrings
 })
+
+await board.runOnce({list:featureV2Strings})
